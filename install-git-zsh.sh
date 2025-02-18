61f1d437ae578854dfb6f937d4510a164ec08785
Hank Do <41064021+hung915@users.noreply.github.com>
2025-02-18 17:07:13 +0700

    Update install-git-zsh.sh

--- install-git-zsh.sh
+++ install-git-zsh.sh
@@ -9,7 +9,3 @@ echo "Start installing sed, zsh, git...\n"
 which sed || sudo apt-get install -y sed
 which zsh || sudo apt-get install -y zsh
 which git || { sudo add-apt-repository ppa:git-core/ppa && sudo apt-get update && sudo apt-get install -y git; }
-
-
-echo "Start installing OhMyZSH...\n"
-[ -d ~/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
