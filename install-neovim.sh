8dd403c9baf1a731a697757ad9faf8c73163292d
Hank Do <41064021+hung915@users.noreply.github.com>
2025-02-20 22:21:09 +0700

    Update install-neovim.sh

--- install-neovim.sh
+++ install-neovim.sh
@@ -4,3 +4,4 @@ set -e  # Exit on error
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
 sudo chmod u+x nvim-linux-x86_64.appimage
 sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+sudo apt install libfuse2
